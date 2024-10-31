f763745a10145e0a4242b823e9c8865ef92b9525
Hongwei Chuang <44053517+Github-Hongwei@users.noreply.github.com>
2024-10-31 20:27:48 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,10 @@ import streamlit as st
 import pandas as pd
 import mplfinance as mpf
 import matplotlib.pyplot as plt
-from pandas_datareader import data as pdr
 
-def get_historical_data(symbol, start_date = None):
-    df = pdr.get_data_yahoo(symbol, start=start_date, end=datetime.now())
-    df = df.rename(columns = {'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj close', 'Volume': 'volume'})
-    for i in df.columns:
-        df[i] = df[i].astype(float)
-    df.index = pd.to_datetime(df.index)
-    if start_date:
-        df = df[df.index >= start_date]
-    return df
-
-hist = get_historical_data('AAPL', '2021-01-01')
+data=yf.download('NVDA')
+data.index = pd.to_datetime(data['timestamp'], unit = 'ms')
 
 fig = mpf.figure(style='yahoo', figsize=(8,6))
-mpf.plot(hist)
+mpf.plot(data)
 st.pyplot(fig)
