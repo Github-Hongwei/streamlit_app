238250fb3c72ae14c3043deb2d3c3e46ebfb4e25
Hongwei Chuang <44053517+Github-Hongwei@users.noreply.github.com>
2024-10-31 20:02:58 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,14 +14,13 @@ st.set_page_config(layout="wide", page_title="WebApp_Demo")
 symbol = st.text_input('Please enter the stock symbol: ', 'NVDA').upper()
 
 st.title(f"{symbol}")
-stock=yf.Ticker(symbol)
-df = stock.history(period="max")
-
-fig = go.Figure(data=[go.Candlestick(x=df.index,
+df=yf.download(symbol,start="2024-01-01",end="2024-09-30")
+st.write(df.describe())
+#fig = go.Figure(data=[go.Candlestick(x=df.index,
                                      open=df['Open'],
                                      high=df['High'],
                                      low=df['Low'],
                                      close=df['Close'])])
 
-fig.update_layout(xaxis_rangeslider_visible=False)
-st.plotly_chart(fig, theme='streamlit')
+#fig.update_layout(xaxis_rangeslider_visible=False)
+#st.plotly_chart(fig, theme='streamlit')
