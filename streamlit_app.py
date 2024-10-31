526d20cc5cb01b4d0f68f7fbf0146c2b6269dcda
Hongwei Chuang <44053517+Github-Hongwei@users.noreply.github.com>
2024-10-31 19:40:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ symbol = st.text_input('Please enter the stock symbol: ', 'NVDA').upper()
 
 st.title(f"{symbol}")
 stock=yf.Ticker(symbol)
-df = stock.history(period='7d')
+df = stock.history(period='5d')
 
 fig = go.Figure(data=[go.Candlestick(x=df.index,
                                      open=df['Open'],
