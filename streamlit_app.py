f117c07e3303ce46a8a9c207a19f61accb57d263
Hongwei Chuang <44053517+Github-Hongwei@users.noreply.github.com>
2024-10-31 20:29:26 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import mplfinance as mpf
 import matplotlib.pyplot as plt
 
 data=yf.download('NVDA')
-data.index = pd.to_datetime(data['timestamp'], unit = 'ms')
-
 fig = mpf.figure(style='yahoo', figsize=(8,6))
 mpf.plot(data)
 st.pyplot(fig)
