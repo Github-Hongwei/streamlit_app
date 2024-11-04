137574c87212edec818abc0c07240da3128a1dfb
Hongwei Chuang <44053517+Github-Hongwei@users.noreply.github.com>
2024-11-04 09:32:00 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,44 @@
-import yfinance as yf
 import streamlit as st
-import mplfinance as mpf
 import matplotlib.pyplot as plt
+import datetime
+import yfinance as yf
+import pandas as pd
+import plotly.graph_objs as go
+import appdirs as ad
+ad.user_cache_dir = lambda *args: "/tmp"
 
-data=yf.download('NVDA')
-st.write(data.describe())
+# Specify title and logo for the webpage.
+# Set up your web app
+st.set_page_config(layout="wide", page_title="WebApp_Demo")
+
+# Sidebar
+st.sidebar.title("Input")
+symbol = st.sidebar.text_input('Please enter the stock symbol: ', 'NVDA').upper()
+# Selection for a specific time frame.
+col1, col2 = st.sidebar.columns(2, gap="medium")
+with col1:
+    sdate = st.date_input('Start Date',value=datetime.date(2024,1,1))
+with col2:
+    edate = st.date_input('End Date',value=datetime.date.today())
+
+st.title(f"{symbol}")
+
+# stock = yf.Ticker(symbol)
+# if stock is not None:
+#   # Display company's basics
+#   st.write(f"# Sector : {stock.info['sector']}")
+#   st.write(f"# Company Beta : {stock.info['beta']}")
+# else:
+#   st.error("Failed to fetch historical data.")
+
+data = yf.download(symbol,start=sdate,end=edate)
+data.columns = pd.Index(['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
+if data is not None:
+  go_fig = go.Figure(data=[go.Candlestick(x=data.index,
+                                          open=data['Open'],
+                                          high=data['High'],
+                                          low=data['Low'],
+                                          close=data['Close'])])
+  st.plotly_chart(go_fig)
+else:
+  st.error("Failed to fetch historical data.")
