01fb4a76f103207fca66e2c4eec6a7c58fcebe5a
Hongwei Chuang <44053517+Github-Hongwei@users.noreply.github.com>
2024-11-04 17:21:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,8 @@ st.set_page_config(layout="wide", page_title="WebApp_Demo")
 # Sidebar
 st.sidebar.title("Input")
 symbol = st.sidebar.text_input('Please enter the stock symbol: ', 'NVDA').upper()
-ma1 = st.sidebar.number_input('Please enter the moving average days: ',value=20)
-ma2 = st.sidebar.number_input('Please enter the moving average days: ',value=50)
+ma1 = st.sidebar.slider('Please enter the moving average days: ',10,200,10)
+ma2 = st.sidebar.slider('Please enter the moving average days: ',50,200,10)
 # Selection for a specific time frame.
 col1, col2 = st.sidebar.columns(2, gap="medium")
 with col1:
