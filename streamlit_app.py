26feddc99440f40ae92eb675a91e1ef775f865ea
Hongwei Chuang <44053517+Github-Hongwei@users.noreply.github.com>
2025-10-22 20:28:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,6 @@ else:
 data = yf.download(symbol,start=sdate,end=edate,multi_level_index=False,auto_adjust=False)
 if data is not None:
     fig=mpf.plot(data,type='candle',style='yahoo',mav=(ma1,ma2),volume=True)
-    st.plot(fig)
+    st.pyplot(fig)
 else:
     st.error("Failed to fetch historical data.")
