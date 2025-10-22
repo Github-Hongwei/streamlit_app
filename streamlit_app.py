c71a9161becb9ebc855fd20460085d0d85a19856
Hongwei Chuang <44053517+Github-Hongwei@users.noreply.github.com>
2025-10-22 20:44:56 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,7 @@ st.title(f"{symbol}")
 
 stock = yf.Ticker(symbol)
 if stock is not None:
-  # Display company's basic
-  st.write(f"# Symbol:{symbol})
+  # Display company's basics
   st.write(f"# Name:{stock.info['shortName']}")
   st.write(f"# Market : {stock.info['market']}")
   st.write(f"# Company Beta : {stock.info['beta']}")
